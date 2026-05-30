f03ceb10d52c7ace99eb2522962093054abd9038
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-30 16:41:21 -0300

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,1 +1,20 @@
 
+function scannerView() {
+  return `
+    <div class="card">
+      <h2>Scanner Expert</h2>
+
+      <button id="startScanner">
+        Iniciar Scanner
+      </button>
+
+      <button id="stopScanner">
+        Parar Scanner
+      </button>
+
+      <p>
+        Scanner ainda não iniciado.
+      </p>
+    </div>
+  `;
+}
