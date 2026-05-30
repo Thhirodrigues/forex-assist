1bf385f99af7a157aaeca65692f8a1aab8ad5cc4
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-30 20:22:57 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,8 @@
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyA6u3X0ClWLL4s6M8lx0dA_82p-AOWRBfE",
+  authDomain: "forex-assist.firebaseapp.com",
+  projectId: "forex-assist",
+  storageBucket: "forex-assist.firebasestorage.app",
+  messagingSenderId: "930868016103",
+  appId: "1:930868016103:web:148d6405e4ac5cc5900342"
+};
