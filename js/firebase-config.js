057f09c5027869bca55e1ce362838ca03ec2eb19
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-05-30 21:19:11 -0300

    Update fireInitialize Firestore
    Create Firebase app and Firestore connection

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,14 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA6u3X0ClWLL4s6M8lx0dA_82p-AOWRBfE",
-  authDomain: "forex-assist.firebaseapp.com",
-  projectId: "forex-assist",
-  storageBucket: "forex-assist.firebasestorage.app",
-  messagingSenderId: "930868016103",
-  appId: "1:930868016103:web:148d6405e4ac5cc5900342"
+    apiKey: "AIzaSyA6u3X0ClWLL4s6M8lxOdA_82p-AOWRBfE",
+    authDomain: "forex-assist.firebaseapp.com",
+    projectId: "forex-assist",
+    storageBucket: "forex-assist.firebasestorage.app",
+    messagingSenderId: "930868016103",
+    appId: "1:930868016103:web:148d6405e4ac5cc5900342"
 };
-window.firebaseConfig = firebaseConfig;
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+window.db = db;
