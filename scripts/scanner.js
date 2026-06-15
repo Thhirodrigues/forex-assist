50990e9f2e8e58aea1c2c0851249b240861bfb23
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-06-15 16:39:04 -0300

    Update scanner.js

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -16,7 +16,9 @@ const {
   ema,
   rsi
 } = require("./utils");
-
+const {
+  calcularQualidade
+} = require("./marketAnalyzer");
 const apiIndex = {
   value: 0
 };
@@ -50,34 +52,6 @@ async function getCandles(symbol) {
 
   return res.data.values.reverse();
 }
-function calcularQualidade(ema9, ema21, rsiAtual) {
-
-  let score = 0;
-
-  const distancia = Math.abs(ema9 - ema21);
-
-  if (distancia > 0.00020)
-    score += 30;
-  else if (distancia > 0.00010)
-    score += 20;
-  else
-    score += 10;
-
-  if (rsiAtual > 55 && rsiAtual < 70)
-    score += 35;
-
-  if (rsiAtual < 45 && rsiAtual > 30)
-    score += 35;
-
-  if (distancia > 0.00030)
-    score += 20;
-
-  if (score > 100)
-    score = 100;
-
-  return score;
-}
-
 async function existeCooldown(par) {
 
   const limite =
