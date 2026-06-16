c5761e0bc14f9cf564d7eb7522b4dcea44b9e7f1
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-06-15 23:34:19 -0300

    Update scanner.js

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -46,20 +46,7 @@ const pares = [
 
 async function getCandles(symbol) {
 
-  const url =
-    `https://api.twelvedata.com/time_series` +
-    `?symbol=${encodeURIComponent(symbol)}` +
-    `&interval=5min` +
-    `&outputsize=120` +
-    '&apikey=${getApiKey(API_KEYS, apiIndex)}'
-
-  const res = await axios.get(url);
-
-  if (!res.data.values)
-    throw new Error("Sem candles");
-
-  return res.data.values.reverse();
-}
+  
 async function existeCooldown(par) {
 
   const limite =
