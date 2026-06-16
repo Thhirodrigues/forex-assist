b5e236e5c4b8aea80f0e18ee80118f8537984e76
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-06-15 23:36:27 -0300

    Implement market data fetching in marketData.js
    Fetch market data using Twelve Data API with axios.

--- scripts/marketData.js
+++ scripts/marketData.js
@@ -1,1 +1,17 @@
-vazio 
+const axios = require("axios");
+
+
+const url =
+    `https://api.twelvedata.com/time_series` +
+    `?symbol=${encodeURIComponent(symbol)}` +
+    `&interval=5min` +
+    `&outputsize=120` +
+    '&apikey=${getApiKey(API_KEYS, apiIndex)}'
+
+  const res = await axios.get(url);
+
+  if (!res.data.values)
+    throw new Error("Sem candles");
+
+  return res.data.values.reverse();
+}
