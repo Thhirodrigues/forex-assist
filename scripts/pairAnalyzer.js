001393b28afcae7308a38370076ea68cdb7c9daf
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-07-02 16:13:02 -0300

    Add alignment output to pairAnalyzer logs

--- scripts/pairAnalyzer.js
+++ scripts/pairAnalyzer.js
@@ -166,6 +166,7 @@ console.log(`EMA21.............${ema21.toFixed(5)}`);
 console.log(`RSI...............${rsiAtual.toFixed(2)}`);
 console.log(`ADX...............${adxAtual.toFixed(2)}`);
 console.log(`Slope............${qualidade.slope}`);
+console.log(`Alinhamento......${qualidade.alinhamento}`);
 console.log(`Score.............${qualidade.score}`);
 console.log(`Qualidade.........${qualidade.qualidade}`);
 console.log("Status............SALVO");
