e6abb393b9af5175c7fffbd26cad911dde951cad
Brothersadega <61800320+Thhirodrigues@users.noreply.github.com>
2026-07-03 21:37:39 -0300

    Refactor imports in scanner.js for clarity

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -43,7 +43,8 @@ const {
     getCandles
 } = require("./marketData");
 const {
-  calcularQualidade
+    calcularQualidade,
+    avaliarOperacao
 } = require("./marketAnalyzer");
 const {
     obterEstatisticasPar
